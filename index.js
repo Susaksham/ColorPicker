c72df3794f1ec5c557063d15ad23af73b1a5721a
Susaksham Jain <sakshamyogesh@gmail.com>
2022-12-07 20:19:13 +0530

    added copyClipboard and styling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.getElementById('btn').addEventListener('click', () => {
   const resultElement = document.getElementById('result')
   const svg = document.getElementsByClassName('svg-container')[0]
+  document.getElementById('btn').classList.toggle('add1')
   console.log(svg)
 
   if (!window.EyeDropper) {
@@ -14,11 +15,35 @@ document.getElementById('btn').addEventListener('click', () => {
   eyeDropper
     .open()
     .then((result) => {
+      document.getElementsByClassName('input')[0].value = result.sRGBHex
       resultElement.textContent = result.sRGBHex
       resultElement.style.backgroundColor = result.sRGBHex
       svg.classList.add('show')
+      document.getElementById('btn').classList.toggle('add1')
     })
     .catch((e) => {
       resultElement.textContent = e
     })
 })
+
+document
+  .querySelector('.svg-container')
+  .addEventListener('click', function myFunction() {
+    // Get the text field
+
+    var copyText = document.querySelector('.input')
+
+    // Select the text field
+    copyText.select()
+    copyText.setSelectionRange(0, 99999) // For mobile devices
+
+    // Copy the text inside the text field
+    document.execCommand('copy')
+    // navigator.clipboard.writeText(copyText.innerText)
+    const svg = document.getElementsByClassName('svg-container')[0]
+    // Alert the copied text
+    svg.classList.add('add')
+    setTimeout(() => {
+      svg.classList.remove('add')
+    }, 500)
+  })
